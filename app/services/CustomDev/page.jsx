c789e9863597d0d7b4bba91172e35ea2c37c2607
mostakin <mostasoft@gmail.com>
2025-09-17 00:20:35 +0600

    Describe changes

--- app/services/CustomDev/page.jsx
+++ app/services/CustomDev/page.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ServiceHero from "./comp/ServiceHero"
+import TechWeWorkWith from './comp/TechWeWorkWith'
+import ServiceHero from './comp/ServiceHero'
+import CliectScetion from './comp/ClientsSection'
+import ServiceDeveloperSce from './comp/serviceDeveloperSec'
 
 const page = () => {
   return (
     <div>
       <ServiceHero/>
+      <CliectScetion/>
+      <ServiceDeveloperSce/>
+      <TechWeWorkWith/>
     </div>
   )
 }
