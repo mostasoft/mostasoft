ffe9e815d0fa072cc004bfdcd8466f1cf4181d1e
mostakin <mostasoft@gmail.com>
2025-09-18 13:50:39 +0600

    Describe changes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -167,7 +167,7 @@ const NavBar = () => {
             Blog
           </Link>
           <Link
-            href="#"
+            href="/contact"
             className="hover:bg-white/30 hover:text-[var(--MostUsed-color)] hover:translate-x-2 transition duration-300 rounded-2xl p-2"
           >
             Contact
